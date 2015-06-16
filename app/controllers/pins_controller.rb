70c54051737ea9d89b5e41c207b14f8f75b95d21
Manuel Lorenzo <mlorenzo@mobiquityinc.com>
2015-06-16 11:02:54 +0200

    editing and improved show

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
-    before_action :find_pin, only: [:show, :dit, :update, :destroy]
+    before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
     def index
         @pins = Pin.all.order("created_at DESC")
     end
@@ -18,10 +19,23 @@ class PinsController < ApplicationController
         end
     end
 
+    def update
+        if @pin.update(pin_params)
+            redirect_to @pin, notice: "Pin was successfully updated"
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @pin.destroy
+        redirect_to root_path
+    end
+
     private
 
     def pin_params
-        params.require(:pin).permit(:title, :descriptions)
+        params.require(:pin).permit(:title, :description)
     end
 
     def find_pin
